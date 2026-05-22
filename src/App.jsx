a8fc47863032913ce3aaeccd0e3f00ccb653872f
gerinpendras-ctrl <gerin.pendras@gmail.com>
2026-05-22 23:10:33 +0800

    added placeholder 3d product view

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <main>
       <Navbar />
       <Hero />
+      <ProductViewer/>
     </main>
   )
 }
